74cdcbba09a3d7fdc245b405e1e43b4a034413c1
Rishil <rishil1211@outlook.com>
2024-07-12 19:20:36 -0400

    Day 30: Return temperature forr any station and date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+
 app = Flask(__name__)
 
 
@@ -11,12 +13,15 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+# this line will concatenate and add str(station) using zfill to add extra 0's in front to get the station id directly
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature =  df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     # Created a directory called templates and the function below will automatically look for the name
     # mentioned in the () inside the templates folder
-    return {"Station": station,
-            "date": date,
-            "temperature": temperature}
+    return {'Station': station,
+            'date': date,
+            'temperature': temperature}
 
 
 if __name__ == "__main__":
