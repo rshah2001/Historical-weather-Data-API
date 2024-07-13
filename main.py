9c7509df47e0c153367bea8913a58f3feb2eb12c
Rishil <rishil1211@outlook.com>
2024-07-12 20:23:36 -0400

    Add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pandas as pd
 app = Flask(__name__)
 
 station = pd.read_csv("data_small/stations.txt", skiprows=17)
-station = station[["STAID","STANAME                                 "]]
+station = station[["STAID", "STANAME                                 "]]
+
 
 @app.route("/")
 def home():
@@ -15,10 +16,11 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-# this line will concatenate and add str(station) using zfill to add extra 0's in front to get the station id directly
+    # this line will concatenate and add str(station) using zfill to add extra 0's in front
+    # to get the station id directly
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    temperature =  df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     # Created a directory called templates and the function below will automatically look for the name
     # mentioned in the () inside the templates folder
     return {'Station': station,
@@ -26,5 +28,21 @@ def about(station, date):
             'temperature': temperature}
 
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=True, port=5001)
